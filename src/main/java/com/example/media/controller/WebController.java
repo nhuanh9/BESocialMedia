0d6c9f5999b3e79dbe96494ee4419a24382f0637
toannvph05233 <johntoan98@gmail.com>
2021-01-12 11:16:50 +0700

    up web socket

--- src/main/java/com/example/media/controller/WebController.java
+++ src/main/java/com/example/media/controller/WebController.java
@@ -2,16 +2,36 @@ package com.example.media.controller;
 
 import com.example.media.model.Hello;
 import com.example.media.model.UserSocket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebController {
+  private String userNameFriend;
+  @Autowired
+  SimpMessagingTemplate simpMessagingTemplate;
  
   @MessageMapping("/hello")
   @SendTo("/topic/hii")
-  public Hello greeting(UserSocket user) throws Exception {
+  public Hello greeting1(UserSocket user) throws Exception {
     return new Hello("Hi, " + user.getName() + "!");
+  }
+
+  @MessageMapping("/chat.newUser")
+  public void greeting(@Payload UserSocket user) {
+    simpMessagingTemplate.convertAndSend("/topic/public/"+ user.getName() + "/" + this.userNameFriend, user.getName() +" : " + user.getMessage());
+    System.out.println("-------------------------------------------");
+    System.out.println("/topic/public/"+ user.getName() + "/" + this.userNameFriend);
+    System.out.println("-------------------------------------------");
+  }
+  @MessageMapping("/chatVsUser")
+  public UserSocket chatVsUser(@Payload UserSocket user, SimpMessageHeaderAccessor headerAccessor) {
+    this.userNameFriend = user.getUserNameFriend();
+    return user;
   }
 }
